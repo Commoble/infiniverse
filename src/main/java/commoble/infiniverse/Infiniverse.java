67ea90847572f31237457bcfb40ec78586ed61d9
Commoble <josephbettendorff@yahoo.com>
2020-12-16 16:30:11 -0600

    Set up workspace

---
+++ src/main/java/commoble/infiniverse/Infiniverse.java
@@ -0,0 +1,15 @@
+package commoble.infiniverse;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Infiniverse.MODID)
+public class Infiniverse
+{
+	public static final String MODID = "infiniverse";
+	
+	// constructor is invoked by forge at start of modloading due to @Mod
+	public Infiniverse()
+	{
+		
+	}
+}
